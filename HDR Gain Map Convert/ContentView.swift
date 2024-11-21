06d89c24661d9091a4d949699461f6e260dc81d2
vincent <vincent_tsang2017@outlook.com>
2024-11-21 16:23:58 +0800

    feat: 实现基本功能

--- HDR Gain Map Convert/ContentView.swift
+++ HDR Gain Map Convert/ContentView.swift
@@ -6,18 +6,207 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
+    @State private var sourceFilePaths: [String] = []
+    @State private var outputDirectoryPath: String = ""
+    @State private var isSingleFileMode: Bool = true // 控制单个文件或多个文件模式
+    @State private var progress: Double = 0.0 // 进度值
+    @State private var isConverting: Bool = false // 是否正在转换
+    private let maxConcurrentTasks = 16 // 最大并发任务数
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $isSingleFileMode) {
+            singleFileView()
+                .tabItem {
+                    Label("单个文件", systemImage: "doc")
+                }
+                .tag(true)
+
+            multipleFilesView()
+                .tabItem {
+                    Label("多个文件", systemImage: "folder")
+                }
+                .tag(false)
         }
         .padding()
     }
+
+    // 单个文件视图
+    private func singleFileView() -> some View {
+        VStack {
+            Text("选择单个源文件")
+                .font(.headline)
+                .padding()
+
+            TextField("源文件路径", text: Binding(
+                get: { sourceFilePaths.first ?? "" },
+                set: { _ in }
+            ))
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            .disabled(true)
+
+            Button("选择源文件") {
+                selectSourceFile()
+            }
+            .padding()
+
+            TextField("输出目录路径", text: $outputDirectoryPath)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .disabled(true)
+
+            Button("选择输出目录") {
+                selectOutputDirectory()
+            }
+            .padding()
+
+            Button("转换图片") {
+                convertImage()
+            }
+            .padding()
+            .disabled(sourceFilePaths.isEmpty || outputDirectoryPath.isEmpty)
+        }
+    }
+
+    // 多个文件视图
+    private func multipleFilesView() -> some View {
+        VStack {
+            Text("选择多个源文件")
+                .font(.headline)
+                .padding()
+
+            List(sourceFilePaths, id: \.self) { path in
+                Text(path)
+            }
+
+            Button("选择多个源文件") {
+                selectMultipleSourceFiles()
+            }
+            .padding()
+
+            TextField("输出目录路径", text: $outputDirectoryPath)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .disabled(true)
+
+            Button("选择输出目录") {
+                selectOutputDirectory()
+            }
+            .padding()
+
+            Button("转换图片") {
+                convertImages()
+            }
+            .padding()
+            .disabled(sourceFilePaths.isEmpty || outputDirectoryPath.isEmpty)
+            
+            if isConverting {
+                ProgressView("转换进度", value: progress, total: Double(sourceFilePaths.count))
+                    .progressViewStyle(LinearProgressViewStyle())
+                    .padding()
+            }
+                
+        }
+    }
+
+    private func selectSourceFile() {
+        let dialog = NSOpenPanel()
+        dialog.title = "选择源文件"
+        dialog.allowedContentTypes = [UTType.tiff, UTType.image, UTType.rawImage]
+        dialog.canChooseDirectories = false
+        dialog.canChooseFiles = true
+
+        dialog.begin { result in
+            if result == .OK, let url = dialog.url {
+                sourceFilePaths = [url.path] // 更新为单个文件路径
+            }
+        }
+    }
+
+    private func selectMultipleSourceFiles() {
+        let dialog = NSOpenPanel()
+        dialog.title = "选择多个源文件"
+        dialog.allowedContentTypes = [UTType.tiff, UTType.image, UTType.rawImage]
+        dialog.canChooseDirectories = false
+        dialog.canChooseFiles = true
+        dialog.allowsMultipleSelection = true // 允许选择多个文件
+
+        dialog.begin { result in
+            if result == .OK {
+                sourceFilePaths = dialog.urls.map { $0.path } // 更新为多个文件路径
+            }
+        }
+    }
+
+    private func selectOutputDirectory() {
+        let dialog = NSOpenPanel()
+        dialog.title = "选择输出目录"
+        dialog.canChooseDirectories = true
+        dialog.canChooseFiles = false
+        dialog.allowsMultipleSelection = false
+
+        dialog.begin { result in
+            if result == .OK, let url = dialog.url {
+                outputDirectoryPath = url.path
+            }
+        }
+    }
+
+    private func convertImage() {
+        // TODO: 在这里实现单个文件的转换逻辑
+        print("源文件: \(sourceFilePaths.first ?? "")")
+        print("输出目录: \(outputDirectoryPath)")
+        let converter = Converter(src: sourceFilePaths.first ?? "", dest: outputDirectoryPath, imageQuality: 1.0, colorSpace: "p3", colorDepth: 10, SDR: false, PQ: false, HLG: false)
+        let result = converter.convert()
+        if result == 0 {
+            print("Converted")
+        } else {
+            print("Failed")
+        }
+    }
+
+    private func convertImages() {
+        isConverting = true
+        progress = 0.0
+        let queue = DispatchQueue(label: "com.example.imageConverter", attributes: .concurrent)
+        let group = DispatchGroup()
+        let semaphore = DispatchSemaphore(value: maxConcurrentTasks) // 创建信号量
+
+        for path in sourceFilePaths {
+            group.enter() // 进入组
+            
+            // 在并发队列中执行转换
+            queue.async {
+                semaphore.wait() // 等待信号量
+
+                let converter = Converter(src: path, dest: outputDirectoryPath, imageQuality: 1.0, colorSpace: "p3", colorDepth: 10, SDR: false, PQ: false, HLG: false)
+                let result = converter.convert()
+                if result == 0 {
+                    print("Converted image: \(path)")
+                } else {
+                    print("Failed to convert image: \(path)")
+                }
+
+                // 更新进度
+                DispatchQueue.main.async {
+                    progress += 1.0
+                }
+
+                semaphore.signal() // 释放信号量
+                group.leave() // 离开组
+            }
+        }
+
+        group.notify(queue: DispatchQueue.main) {
+            isConverting = false
+            print("输出目录: \(outputDirectoryPath)")
+        }
+    }
 }
+
 
 #Preview {
     ContentView()
---
+++ HDR Gain Map Convert/Library/PQHDR_to_GainMapHDR.swift
@@ -0,0 +1,186 @@
+//
+//  PQHDR_to_GainMapHDR.swift
+//  HDR Gain Map Convert
+//
+//  Created by Vincent Tsang on 21/11/2024.
+//
+
+//
+//  main.swift
+//  PQHDR_to_GainMapHDR
+//  This code will convert PQ HDR file to luminance gain map HDR heic file.
+//
+//  Created by Luyao Peng on 2024/9/27.
+//
+
+import CoreImage
+import Foundation
+
+class Converter {
+    var src: String
+    var dest: String
+    var imageQuality: Double
+    var colorSpace: String
+    var colorDepth: Int
+    var SDR: Bool
+    var PQ: Bool
+    var HLG: Bool
+    
+    init(src: String, dest: String, imageQuality: Double, colorSpace: String, colorDepth: Int, SDR: Bool, PQ: Bool, HLG: Bool) {
+        self.src = src
+        self.dest = dest
+        self.imageQuality = imageQuality
+        self.colorSpace = colorSpace
+        self.colorDepth = colorDepth
+        self.SDR = SDR
+        self.PQ = PQ
+        self.HLG = HLG
+    }
+    
+    func convert() -> Int {
+        let ctx = CIContext()
+        
+        /*
+         let arguments = CommandLine.arguments
+         guard arguments.count > 2 else {
+         print("Usage: PQHDRtoGMHDR <source file> <destination folder> <options>\n       options:\n         -q <value>: image quality (default: 0.85)\n         -c <color space>: specify output color space (srgb, p3, rec2020)\n         -d <color depth>: specify output color depth (default: 8)\n         -s: export tone mapped SDR image without HDR gain map\n         -p: export 10bit PQ HDR heic image")
+         exit(1)
+         }
+         */
+        
+        let url_hdr = URL(fileURLWithPath: self.src)
+        let filename = url_hdr.deletingPathExtension().appendingPathExtension("heic").lastPathComponent
+        let path_export = URL(fileURLWithPath: self.dest)
+        let url_export_heic = path_export.appendingPathComponent(filename)
+        //let imageoptions
+        
+        var imagequality: Double? = 0.85
+        var sdr_export: Bool = false
+        var pq_export: Bool = false
+        var hlg_export: Bool = false
+        var bit_depth = CIFormat.RGBA8
+        
+        let hdr_image = CIImage(contentsOf: url_hdr, options: [.expandToHDR: true])
+        let tonemapped_sdrimage = hdr_image?.applyingFilter("CIToneMapHeadroom", parameters: ["inputTargetHeadroom":1.0])
+        let export_options = NSDictionary(dictionary:[kCGImageDestinationLossyCompressionQuality:imagequality ?? 0.85, CIImageRepresentationOption.hdrImage:hdr_image!])
+        
+        var sdr_color_space = CGColorSpace.displayP3
+        var hdr_color_space = CGColorSpace.displayP3_PQ
+        var hlg_color_space = CGColorSpace.displayP3_HLG
+        
+        let image_color_space = hdr_image?.colorSpace?.name
+        if (image_color_space! as NSString).contains("709") {
+            sdr_color_space = CGColorSpace.itur_709
+            hdr_color_space = CGColorSpace.itur_709_PQ
+            hlg_color_space = CGColorSpace.itur_709_HLG
+        }
+        if (image_color_space! as NSString).contains("sRGB") {
+            sdr_color_space = CGColorSpace.itur_709
+            hdr_color_space = CGColorSpace.itur_709_PQ
+            hlg_color_space = CGColorSpace.itur_709_HLG
+        }
+        if (image_color_space! as NSString).contains("2100") {
+            sdr_color_space = CGColorSpace.itur_2020_sRGBGamma
+            hdr_color_space = CGColorSpace.itur_2100_PQ
+            hlg_color_space = CGColorSpace.itur_2100_HLG
+        }
+        if (image_color_space! as NSString).contains("2020") {
+            sdr_color_space = CGColorSpace.itur_2020_sRGBGamma
+            hdr_color_space = CGColorSpace.itur_2100_PQ
+            hlg_color_space = CGColorSpace.itur_2100_HLG
+        }
+        
+        if self.imageQuality > 1 {
+            imagequality = self.imageQuality / 100
+        } else {
+            imagequality = self.imageQuality
+        }
+        
+        
+        if self.SDR {
+            if self.PQ || self.HLG {
+                debugPrint("Error: Only one type of export can be specified.")
+                return -1
+            }
+            sdr_export = true
+        }
+        
+        if self.PQ {
+            if self.SDR || self.HLG {
+                debugPrint("Error: Only one type of export can be specified.")
+                return -1
+            }
+            pq_export = true
+        }
+        
+        if self.HLG {
+            if self.SDR || self.PQ {
+                debugPrint("Error: Only one type of export can be specified.")
+                return -1
+            }
+            hlg_export = true
+        }
+        
+        if self.colorDepth == 10 {
+            bit_depth = CIFormat.RGB10
+        }
+        
+        switch self.colorSpace {
+        case "srgb","709","rec709","rec.709","bt709","bt,709","itu709":
+            sdr_color_space = CGColorSpace.itur_709
+            hdr_color_space = CGColorSpace.itur_709_PQ
+            hlg_color_space = CGColorSpace.itur_709_HLG
+        case "p3","dcip3","dci-p3","dci.p3","displayp3":
+            sdr_color_space = CGColorSpace.displayP3
+            hdr_color_space = CGColorSpace.displayP3_PQ
+            hlg_color_space = CGColorSpace.displayP3_HLG
+        case "rec2020","2020","rec.2020","bt2020","itu2020","2100","rec2100","rec.2100":
+            sdr_color_space = CGColorSpace.itur_2020_sRGBGamma
+            hdr_color_space = CGColorSpace.itur_2100_PQ
+            hlg_color_space = CGColorSpace.itur_2100_HLG
+        default:
+            debugPrint("Error: The colorSpace argument requires color space argument. (srgb, p3, rec2020)")
+        }
+        
+        
+        while sdr_export {
+            let sdr_export_options = NSDictionary(dictionary:[kCGImageDestinationLossyCompressionQuality:imagequality ?? 0.85])
+            try! ctx.writeHEIFRepresentation(of: tonemapped_sdrimage!,
+                                             to: url_export_heic,
+                                             format: bit_depth,
+                                             colorSpace: CGColorSpace(name: sdr_color_space)!,
+                                             options:sdr_export_options as! [CIImageRepresentationOption : Any])
+            return 0
+        }
+        
+        while hlg_export {
+            let hlg_export_options = NSDictionary(dictionary:[kCGImageDestinationLossyCompressionQuality:imagequality ?? 0.85])
+            try! ctx.writeHEIFRepresentation(of: hdr_image!,
+                                             to: url_export_heic,
+                                             format: bit_depth,
+                                             colorSpace: CGColorSpace(name: hlg_color_space)!,
+                                             options:hlg_export_options as! [CIImageRepresentationOption : Any])
+            return 0
+        }
+        
+        while pq_export {
+            let pq_export_options = NSDictionary(dictionary:[kCGImageDestinationLossyCompressionQuality:imagequality ?? 0.85])
+            try! ctx.writeHEIF10Representation(of: hdr_image!,
+                                               to: url_export_heic,
+                                               colorSpace: CGColorSpace(name: hdr_color_space)!,
+                                               options:pq_export_options as! [CIImageRepresentationOption : Any])
+            return 0
+        }
+        
+        try! ctx.writeHEIFRepresentation(of: tonemapped_sdrimage!,
+                                         to: url_export_heic,
+                                         format: bit_depth,
+                                         colorSpace: CGColorSpace(name: sdr_color_space)!,
+                                         options: export_options as! [CIImageRepresentationOption : Any])
+        return 0
+    }
+}
+
+
+
+
