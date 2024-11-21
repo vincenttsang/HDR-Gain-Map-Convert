bceeef1a8dfa0854e57973238e5586d91a343b05
vincent <vincent_tsang2017@outlook.com>
2024-11-21 14:50:45 +0800

    Initial Commit

---
+++ HDR Gain Map Convert/HDR_Gain_Map_ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDR_Gain_Map_ConvertApp.swift
+//  HDR Gain Map Convert
+//
+//  Created by Vincent Tsang on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HDR_Gain_Map_ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HDR Gain Map ConvertUITests/HDR_Gain_Map_ConvertUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HDR_Gain_Map_ConvertUITestsLaunchTests.swift
+//  HDR Gain Map ConvertUITests
+//
+//  Created by Vincent Tsang on 21/11/2024.
+//
+
+import XCTest
+
+final class HDR_Gain_Map_ConvertUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
